c22003e16fdebf0ada3d2f618062bde26787aec8
Stanislav Semenchuk <semenchuksu@yandex.ru>
2025-05-02 12:01:06 +0300

    hw08: config.c clean

--- hw08-daemons/src/config.c
+++ hw08-daemons/src/config.c
@@ -8,6 +8,7 @@
 #include "error_codes.h"
 
 static void parse(const char* path, struct config* config);
+static void add_to_config(char* param, char* value, struct config* config);
 
 int create_config(const char* path, struct config* config)
 {
@@ -59,18 +60,7 @@ static void parse(const char* path, struct config* config)
         strncpy(value, line + i, strlen(line) - i - 1);
         value[strlen(line) - i - 1] = 0;
 
-        if (strcmp("filepath", param) == 0) {
-          config->filepath = malloc(strlen(value));
-          strcpy(config->filepath, value);
-        } else if (strcmp("daemonized", param) == 0) {
-          config->demonized = strcmp(value, "true") == 0 ? true : false;
-        } else if (strcmp("logfile", param) == 0) {
-          config->logfile = malloc(strlen(value));
-          strcpy(config->logfile, value);
-        } else if (strcmp("homedir", param) == 0) {
-          config->homedir = malloc(strlen(value));
-          strcpy(config->homedir, value);
-        }
+        add_to_config(param, value, config);
       }
     }
   }
@@ -79,4 +69,20 @@ static void parse(const char* path, struct config* config)
     free(line);
   }
   fclose(file);
+}
+
+static void add_to_config(char* param, char* value, struct config* config)
+{
+  if (strcmp("filepath", param) == 0) {
+    config->filepath = malloc(strlen(value));
+    strcpy(config->filepath, value);
+  } else if (strcmp("daemonized", param) == 0) {
+    config->demonized = strcmp(value, "true") == 0 ? true : false;
+  } else if (strcmp("logfile", param) == 0) {
+    config->logfile = malloc(strlen(value));
+    strcpy(config->logfile, value);
+  } else if (strcmp("homedir", param) == 0) {
+    config->homedir = malloc(strlen(value));
+    strcpy(config->homedir, value);
+  }
 }
